835126155b80743598d19df8e67bfa98b5734dbc
HichemELB <hichem@sendinblue.com>
2023-01-01 23:03:25 +0100

    Add play time to fact user_game

--- dbt/models/gold/fact_user_game.sql
+++ dbt/models/gold/fact_user_game.sql
@@ -8,6 +8,7 @@ WITH game_user as (
         , dim_users.user_id
         , DATE(dim_users.intial_game_utc_at) AS intial_game_utc_at
         , dim_games.game_id
+        , dim_games.play_time
         --, COUNT(dim_games.game_id) AS played_games_count
         --, sum(dim_games.play_time) AS played_time_total
     FROM {{ ref("dim_users") }} dim_users
@@ -20,6 +21,7 @@ WITH game_user as (
         , dim_users.user_id
         , DATE(dim_users.intial_game_utc_at) AS intial_game_utc_at
         , dim_games.game_id
+        , dim_games.play_time
         --, COUNT(dim_games.game_id) AS played_games_count
         --, sum(dim_games.play_time) AS played_time_total
     FROM {{ ref("dim_users") }} dim_users
